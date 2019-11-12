3b83e86dcae2337cf5267137a8c93cc0fd98747f
Dustin Choat <dustinchoat@mymail.mines.edu>
2019-11-12 11:40:39 -0700

    created file that calculates the first 10 Fibonacci numbers

---
+++ fibonacci.py
@@ -0,0 +1,30 @@
+# fibonacci.py
+
+def fib():
+    fibs = [1, 2]
+    count = 1
+    num = 0
+    cant = 0
+    fibs = []
+    for i in range(1,9):
+
+
+        while num < 10:
+            #print (count)
+            num = num + 1
+            cont = cant
+            cant = count
+            count = cont + cant
+            fibs.append(count)
+        ''' 
+        implement Fibonacci sequence to calculate the 
+        first 10 Fibonacci numbers, note Fn = Fn-1 + Fn-2
+        '''
+
+    return fibs
+
+def main():
+    print('OUTPUT', fib())
+
+if __name__ == "__main__":
+    main()
